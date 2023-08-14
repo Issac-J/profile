763edebfb7e492cb1d5444ce2fb55584c5bb30f8
EggsyJ <fordev0905@gmail.com>
2023-08-14 23:30:43 +0900

    create Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import "./styles/App.css";
-import About from "./components/About";
-import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-import Portfolios from "./components/Portfolios";
+// import About from "./components/About";
+// import Contact from "./components/Contact";
+// import Portfolios from "./components/Portfolios";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <About />
-      <Portfolios />
-      <Contact />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { Link } from "react-scroll";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "../styles/Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav id="navbar">
+      <div className="container">
+        {/* eslint-disable-next-line */}
+        <a href="#" className="Resume">
+          My Resume
+        </a>
+      </div>
+
+      <a
+        href="https://github.com/Issac-J"
+        target="_blank"
+        rel="noreferrer"
+        className="icons"
+      >
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+
+      <ul className="nav-links">
+        <div className="name-box">
+          <li className="Name">
+            A <span className="period">.</span>
+          </li>
+        </div>
+        <Link to="About" smooth={true} duration={500}>
+          <li>About</li>
+        </Link>
+        <Link to="Projects" smooth={true} duration={500}>
+          <li>Projects</li>
+        </Link>
+        <Link to="Contact" smooth={true} duration={500}>
+          <li>Contact</li>
+        </Link>
+      </ul>
+
+      <div className="Introduction">
+        <h1>Hey, I'm Akshay</h1>
+        <h2>
+          <span></span>
+        </h2>
+        <p>
+          I'm currently a Full Stack Developer seeking many ways to improve my
+          skills through problem-solving and creating various projects! Scroll
+          down to learn more about me.
+        </p>
+        <Link to="Contact" smooth={true} duration={900}>
+          <button>Contact Me</button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
